27376e5b2255b88d8b3e0857335deeede06b2956
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-02-11 14:05:24 +0000

    get courses by star rating

--- src/main/java/com/example/codeclan/courseserivce/repositories/CourseRepository.java
+++ src/main/java/com/example/codeclan/courseserivce/repositories/CourseRepository.java
@@ -4,6 +4,11 @@ import com.example.codeclan.courseserivce.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findByStarRating(int starRating);
+
 }
